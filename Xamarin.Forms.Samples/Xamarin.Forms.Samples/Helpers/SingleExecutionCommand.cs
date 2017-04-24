069b5621c31861cccc3f01dda97ffc27ac4da67f
Stayrony <beyoncem7@gmail.com>
2017-04-24 12:19:54 +0300

    add SingleExecutionCommand

---
+++ Xamarin.Forms.Samples/Xamarin.Forms.Samples/Helpers/SingleExecutionCommand.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Xamarin.Forms.Samples.Helpers
+{
+    public class SingleExecutionCommand : ICommand
+    {
+        private Func<object, Task> _func;
+        private bool _isExecuting;
+        private int _delayMillisec;
+
+        private SingleExecutionCommand()
+        {
+
+        }
+
+        public static SingleExecutionCommand FromFunc(Func<Task> func, int delayMillisec = 400)
+        {
+            var ret = new SingleExecutionCommand();
+            ret._func = (obj) =>
+            {
+                return func();
+            };
+            ret._delayMillisec = delayMillisec;
+            return ret;
+        }
+
+        public static SingleExecutionCommand FromFunc(Func<object, Task> func, int delayMillisec = 400)
+        {
+            var ret = new SingleExecutionCommand();
+            ret._func = func;
+            ret._delayMillisec = delayMillisec;
+            return ret;
+        }
+
+        public static SingleExecutionCommand FromFunc<T>(Func<T, Task> func, int delayMillisec = 400)
+        {
+            var ret = new SingleExecutionCommand();
+            ret._func = (object obj) =>
+            {
+                var objT = default(T);
+                objT = (T)obj;
+                return func(objT);
+            };
+            ret._delayMillisec = delayMillisec;
+            return ret;
+        }
+
+        #region -- ICommand implementation --
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            //TODO: improve it
+            return true;
+        }
+
+        public async void Execute(object parameter)
+        {
+            if (_isExecuting)
+                return;
+            _isExecuting = true;
+            await _func(parameter);
+            if (_delayMillisec > 0)
+                await Task.Delay(_delayMillisec);
+            _isExecuting = false;
+        }
+
+        #endregion
+    }
+}
